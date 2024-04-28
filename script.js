d39f284e86fbfe39f5c362248edef79cccd40314
Antony Petsas <152808269+TKP888@users.noreply.github.com>
2024-04-28 12:08:31 +0100

    js changed to use class
    size of main container to fix once book is submitted

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    toggleRead() {
+        this.read = !this.read;
+    }
+}
+
 
 let myLibrary = [];
 
 
 
-function Book(title, author, pages, read) {
- this.title = title;
- this.author = author;
- this.pages = pages;
- this.read = read;
-};
-
-// Book.prototype.getDetails = function(){
-//     return this.title +' by '+ this.author +', '+ this.pages+', '+ this.read+'.';
-// }
-
-// const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
-// const book2 = new Book('Fourth Wing', 'Rebecca Yarros', '651 pages', 'mid-read');
-
-// console.log(book1);
-
-// console.log(book2.getDetails());
-
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
-}
-
 function toggleRead(index){
     myLibrary[index].toggleRead();
     render();
 }
-
 
 function render(){
     let libraryEl = document.querySelector("#library");
@@ -56,34 +45,30 @@ function render(){
     }
 }
 
-
 function removeBook(index) {
 myLibrary.splice(index, 1);
 render();
 }
 
-function addBookToLibrary() {
-    // do stuff here
-let title = document.querySelector("#title").value;
-let author = document.getElementById("author").value;
-let pages = document.getElementById("pages").value;
-let read = document.getElementById("read").checked;
-let newBook = new Book(title, author, pages, read);
-myLibrary.push(newBook);
-render();
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    render();
+}
 
-  }
-
-  let newBookBtn =  document.querySelector("#newBookBtn");
-  newBookBtn.addEventListener ("click", function() {
-
+let newBookBtn = document.querySelector("#newBookBtn");
+newBookBtn.addEventListener("click", function () {
     let newBookForm = document.querySelector("#newBookForm");
-    console.log(newBookForm);
     newBookForm.style.display = "grid";
-})
+});
 
-document.querySelector("#newBookForm").addEventListener("submit", function(event){
-event.preventDefault();
-addBookToLibrary();
+ 
 
-})
+document.querySelector("#newBookForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+       let title = document.querySelector("#title").value;
+       let author = document.getElementById("author").value;
+       let pages = document.getElementById("pages").value;
+       let read = document.getElementById("read").checked;
+    addBookToLibrary(title, author, pages, read);
+});
